07135706dde42f64700a714229252581a34dd15a
Eugenio Mendoza Robaina <eu.mendoza@gmail.com>
2015-04-10 09:52:48 +0200

    Create AlumnosController

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var myApp = angular.module("myApp",[]);
+
+myApp.controller("AlumnosController",["$scope",function($scope){
+	$scope.alumnos = [
+		{nombre:"Eugenio Mendoza", telefono:"666999666", curso:"Primero ESO"},
+		{nombre:"Eduardo Mendoza", telefono:"666999666", curso:"Segundo ESO"},
+		{nombre:"Carlos Mendoza", telefono:"666999666", curso:"Primero ESO"},
+		{nombre:"Enrique Mendoza", telefono:"666999666", curso:"Cuarto ESO"},
+		{nombre:"David Mendoza", telefono:"666999666", curso:"Tercero ESO"}
+	];
+}]);
